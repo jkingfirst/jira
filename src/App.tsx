effbb3e56f3cc333b85ab1bb642959b86fca6e4d
墨金 <jinyuanchengking@sina.com>
2021-06-28 22:35:04 +0800

    测试 pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { add } from "./utils/tools";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      {add(1, 2)}
     </div>
   );
 }
